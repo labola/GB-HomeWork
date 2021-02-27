63d38c9d224f5049e22f5ba7ada9a16713b7e896
Mikhail <labola2016@yandex.ru>
2021-02-28 01:44:59 +0300

    add main and external values

---
+++ HW3/build/CMakeFiles/main.dir/main.cpp.obj.d
@@ -0,0 +1,118 @@
+CMakeFiles/main.dir/main.cpp.obj: \
+  C:\Users\User\Documents\GB\GB-HomeWork\HW3\main.cpp \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\iostream \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\yvals_core.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vcruntime.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\sal.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\concurrencysal.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\vadefs.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vadefs.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xkeycheck.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\istream \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\ostream \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\ios \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xlocnum \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\climits \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\limits.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\limits.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cmath \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\yvals.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\crtdbg.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vcruntime_new_debug.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vcruntime_new.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\crtdefs.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\use_ansi.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cstdlib \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\math.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_math.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\stdlib.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_malloc.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_search.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\stddef.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\__stddef_max_align_t.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wstdlib.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xtr1common \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cstdio \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\stdio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wstdio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_stdio_config.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\iterator \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\iosfwd \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cstring \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\string.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_memory.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_memcpy_s.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\errno.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vcruntime_string.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wstring.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cwchar \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\wchar.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wconio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wctype.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wdirect.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_share.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wprocess.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_wtime.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\sys\stat.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\sys\types.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xstddef \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cstddef \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\initializer_list \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xutility \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\utility \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\type_traits \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\streambuf \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xiosbase \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\share.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\system_error \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\__msvc_system_error_abi.hpp \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cerrno \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\stdexcept \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\exception \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\malloc.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vcruntime_exception.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\eh.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\corecrt_terminate.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xstring \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xmemory \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cstdint \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\stdint.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\stdint.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\limits \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\cfloat \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\float.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\float.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\intrin0.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\intrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\x86intrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\ia32intrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\immintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\mmintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\xmmintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\mm_malloc.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\emmintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\bmiintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\fxsrintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\xsaveintrin.h \
+  C:\Program\ Files\LLVM\lib\clang\11.0.0\include\adxintrin.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\setjmp.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\isa_availability.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\new \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xatomic.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xcall_once.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xerrc.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\atomic \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xthreads.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xtimec.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\time.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xlocale \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\memory \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\typeinfo \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\vcruntime_typeinfo.h \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xfacet \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xlocinfo \
+  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2019\BuildTools\VC\Tools\MSVC\14.28.29333\include\xlocinfo.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\ctype.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.18362.0\ucrt\locale.h
