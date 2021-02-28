977dffd5efba1129ef0f9a73e82de2eb07b55294
Mikhail <labola2016@yandex.ru>
2021-02-28 03:09:58 +0300

    add second solution

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -8,9 +8,9 @@ int main() {
     int x = 50;
     std::cout << ((static_cast<float>(a) * (static_cast<float>(b) + (static_cast<float>(c) / static_cast<float>(d))))) << '\n';
 
-    int y = x-21;
-    std::cout << y << '\n';
+    int y= x-21;
+    std::cout << y  << '\n';
     std::cout << ((x-21) > 21 ? y *= y : y);
 
-    
+
 }
