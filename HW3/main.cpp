ad4e35df5a35b25d759142f545b93ca703da9bdf
Mikhail <labola2016@yandex.ru>
2021-03-03 00:29:04 +0300

    add 4 solution

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -6,11 +6,23 @@ extern int a,b,c,d;
 int main() {
  
     int x = 50;
+    //Exercise 1 included. 
     std::cout << ((static_cast<float>(a) * (static_cast<float>(b) + (static_cast<float>(c) / static_cast<float>(d))))) << '\n';
 
     int y= x-21;
     std::cout << y  << '\n';
     std::cout << ((x-21) > 21 ? y *= y : y);
 
+    std::cout << '\n';
+    
+    int array[3][3][3]={
+        {{1,1,1}, {1,1,1},{1,1,1}},
+        {{2,2,2}, {2,8,2},{2,2,2}},
+        {{3,3,3}, {3,3,3},{3,3,3}},
+    };
+
+    int* ptr = array[1][1];
+        
+    std::cout << *(ptr+1);
 
 }
